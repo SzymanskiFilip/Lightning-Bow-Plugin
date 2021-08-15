6ed5f261affa65f9584a47bad7dedeced9d9d026
Filip <szymanski.fp@gmail.com>
2021-08-15 19:51:28 +0200

    added the event to the plugin manager in the server

--- src/main/java/eu/filipszymanski/lightningbowplugin/LightningBowPlugin.java
+++ src/main/java/eu/filipszymanski/lightningbowplugin/LightningBowPlugin.java
@@ -1,5 +1,6 @@
 package eu.filipszymanski.lightningbowplugin;
 
+import eu.filipszymanski.lightningbowplugin.events.LightningBowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LightningBowPlugin extends JavaPlugin {
@@ -7,8 +8,9 @@ public final class LightningBowPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("###LIGHTNING-BOW SUCCESSFULY ACTIVATED!###");
+
+        getServer().getPluginManager().registerEvents(new LightningBowEvent(), this);
         getCommand("lbow").setExecutor(new BowCommand());
     }
-
-
+    
 }
