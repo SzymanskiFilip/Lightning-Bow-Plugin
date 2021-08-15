ab767dab69a5c59324e44205dfc66512b93a89b5
Filip <szymanski.fp@gmail.com>
2021-08-15 19:50:53 +0200

    made the lightingstrike bow only for operators

---
+++ src/main/java/eu/filipszymanski/lightningbowplugin/events/LightningBowEvent.java
@@ -0,0 +1,30 @@
+package eu.filipszymanski.lightningbowplugin.events;
+
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class LightningBowEvent implements Listener {
+
+    @EventHandler
+    public void onShoot(ProjectileHitEvent e){
+        if(e.getEntity() instanceof Arrow){
+            if(e.getEntity().getShooter() instanceof Player){
+                Player player = (Player) e.getEntity().getShooter();
+                if(player.isOp()){
+                    Location location = e.getEntity().getLocation();
+
+                    player.sendMessage("PROJECTILE LANDED");
+                    location.getWorld().strikeLightning(location);
+                    location.getWorld().playSound(location, Sound.BLOCK_SNOW_HIT, 10.0f, 1.0f);
+                }
+            }
+
+        }
+    }
+
+}
